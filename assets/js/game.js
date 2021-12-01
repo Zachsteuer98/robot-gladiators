a551a60a36014714a686a030a32251a572c8e84f
Zachsteuer98 <Zach.Steuer98@gmail.com>
2021-12-01 18:37:27 -0500

    Created first variable and tested it in the console. Then I added the first function which is fight.

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+var playerName = window.prompt("what is your robot's name?");
+
+console.log(playerName);
+
+console.log("Our robot's name is " + playerName);
+// this creates a function named "fight" 
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
